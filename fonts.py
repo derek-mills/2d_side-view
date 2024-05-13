3f6545226b7400daec6b73795d2e5fed2e6a806c
sid <alexsidelnikov@yandex.ru>
2024-05-13 17:19:17 +0800

    added some files

---
+++ fonts.py
@@ -0,0 +1,85 @@
+import pygame
+
+# Инициализация шрифтов:
+pygame.font.init()
+# font6 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 6)
+# font8 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 8)
+# font10 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 10)
+# font12 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 12)
+# font14 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 14)
+# font16 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 16)
+# font18 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 18)
+# font15 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 15)
+# font20 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 20)
+# font30 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/n/nes.ttf", 30)
+
+# font6 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 6)
+# font8 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 8)
+# font10 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 10)
+# font12 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 12)
+# font14 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 14)
+# font16 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 16)
+# font18 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 18)
+# font15 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 15)
+# font20 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 20)
+# font25 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 25)
+# font30 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 30)
+# font40 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 40)
+# font50 = pygame.font.Font("/media/sid/d90c7353-09c5-492a-aa88-66d695362d79/sid/.fonts/a/AGHCB.ttf", 45)
+# font6 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 6)
+# font8 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 8)
+# font10 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 10)
+# font12 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 12)
+# font14 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 14)
+# font16 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 16)
+# font18 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 18)
+# font15 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 15)
+# font20 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 20)
+# font25 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 25)
+# font30 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 30)
+# font40 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 40)
+# font50 = pygame.font.Font("./fonts/AlbionicRegular.ttf", 45)
+# font_name = "./fonts/HARDLIFE.TTF"
+# font_name = "./fonts/COPRGTL.TTF"
+# font_name = "./fonts/diploma.TTF"
+font_name = "./fonts/AGHCB.ttf"
+# font_name = "./fonts/VOGUB___.TTF"
+# font_name = "./fonts/AlbionicRegular.ttf"
+font6 = pygame.font.Font(font_name, 6)
+font8 = pygame.font.Font(font_name, 8)
+font10 = pygame.font.Font(font_name, 10)
+font12 = pygame.font.Font(font_name, 12)
+font14 = pygame.font.Font(font_name, 14)
+font16 = pygame.font.Font(font_name, 16)
+font18 = pygame.font.Font(font_name, 18)
+font15 = pygame.font.Font(font_name, 15)
+font20 = pygame.font.Font(font_name, 20)
+font25 = pygame.font.Font(font_name, 25)
+font30 = pygame.font.Font(font_name, 30)
+font40 = pygame.font.Font(font_name, 40)
+font50 = pygame.font.Font(font_name, 50)
+font60 = pygame.font.Font(font_name, 60)
+font70 = pygame.font.Font(font_name, 70)
+font80 = pygame.font.Font(font_name, 80)
+font90 = pygame.font.Font(font_name, 90)
+font100 = pygame.font.Font(font_name, 100)
+all_fonts = {
+    6: font6,
+    8: font8,
+    10: font10,
+    12: font12,
+    14: font14,
+    15: font15,
+    16: font16,
+    18: font18,
+    20: font20,
+    25: font25,
+    30: font30,
+    40: font40,
+    50: font50,
+    60: font60,
+    70: font70,
+    80: font80,
+    90: font90,
+    100: font100,
+}
