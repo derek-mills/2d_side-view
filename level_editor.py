c62ff5edc6d8edcb841fabd140113ac91b6e7263
sid <alexsidelnikov@yandex.ru>
2024-05-20 15:55:59 +0800

    Added level editor

---
+++ level_editor.py
@@ -0,0 +1,242 @@
+from constants import *
+# from world import *
+from locations import *
+from obstacle import *
+import camera
+# from sound import *
+# import fonts
+
+class World(object):
+    def __init__(self):
+        # CONTROLS
+        self.is_key_pressed = False
+        self.is_input_up_arrow = False
+        self.is_input_down_arrow = False
+        self.is_input_right_arrow = False
+        self.is_input_left_arrow = False
+        self.is_input_confirm = False
+        self.is_input_cancel = False
+        self.is_z = False
+        self.is_p = False
+        self.is_i = False
+        self.is_x = False
+        self.is_n = False
+        self.is_b = False
+        self.is_spacebar = False
+        self.is_l_shift = False
+        self.is_l_ctrl = False
+        self.is_l_alt = False
+        self.is_mouse_button_down = False
+        self.is_left_mouse_button_down = False
+        self.is_right_mouse_button_down = False
+        self.is_mouse_wheel_rolls = False
+        self.is_mouse_wheel_up = False
+        self.is_mouse_wheel_down = False
+        self.mouse_xy = list()  #
+        self.is_mouse_hovers_item: bool = False
+        self.mouse_hovers_item: int = 0
+        self.is_mouse_hovers_actor: bool = False
+        self.mouse_hovers_actor: int = 0
+        self.camera_follows_mouse = True
+
+        self.obstacles = dict()
+        self.obstacle_id: int = 0
+        self.location = '01'
+        self.screen = None
+        self.camera = camera.Camera()
+        self.camera.setup(MAXX*2, MAXY)
+
+
+    def set_screen(self, surface):
+        self.screen = surface
+
+    def processing_human_input(self):
+        self.mouse_xy = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                raise SystemExit()
+            # mods = pygame.key.get_mods()
+            # if mods & KMOD_LSHIFT:  # use whatever KMOD_ constant you want;)
+            #     self.is_l_shift = True
+            # elif mods & KMOD_LCTRL:
+            #     self.is_l_ctrl = True
+            # elif mods & KMOD_LALT:
+            #     self.is_l_alt = True
+            # else:
+            #     self.is_l_ctrl = False
+            #     self.is_l_shift = False
+            #     self.is_l_alt = False
+            # # print(self.l_shift)
+            # if event.type == KEYUP:
+            #     self.is_key_pressed = False
+            #     if event.key == K_d:
+            #         self.is_input_right_arrow = False
+            #     if event.key == K_a:
+            #         self.is_input_left_arrow = False
+            #     if event.key == K_w:
+            #         self.is_input_up_arrow = False
+            #     if event.key == K_s:
+            #         self.is_input_down_arrow = False
+            #     if event.key == K_SPACE:
+            #         self.is_spacebar = False
+            #     # elif event.key == K_z:
+            #     #     self.z = False
+            #     # elif event.key == K_x:
+            #     #     self.input_cancel = False
+            #     # if event.key == K_KP_PLUS:
+            #     #     self.avatars_row_scale_decrease = False
+            #     # if event.key == K_KP_MINUS:
+            #     #     self.avatars_row_scale_increase = False
+            if event.type == KEYDOWN:
+                self.is_key_pressed = True
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    raise SystemExit()
+            #
+                if event.key == K_l:
+                    self.load()
+            #         self.is_input_right_arrow = True
+            #     if event.key == K_a:
+            #         self.is_input_left_arrow = True
+            #     if event.key == K_w:
+            #         self.is_input_up_arrow = True
+            #     if event.key == K_s:
+            #         self.is_input_down_arrow = True
+            #     if event.key == K_SPACE:
+            #         self.is_spacebar = True
+            #     # if event.key == K_F5:
+            #     #     self.need_quick_save = True
+            #     # elif event.key == K_F8:
+            #     #     self.need_quick_load = True
+            #     #     # quick_save(self, self.locations)
+            #     # elif event.key == K_F3:
+            #     #     self.music_on = False if self.music_on else True
+            #     #     if not self.music_on:
+            #     #         pygame.mixer.music.fadeout(2000)
+            #     elif event.key == K_z:
+            #         # Cool stuff with if-then-else expression compress:
+            #         self.is_z = False if self.is_z else True
+            #     elif event.key == K_x:
+            #         # self.change_mode()
+            #         self.is_x = False if self.is_x else True
+            #         # self.screen_follows_actor = False if self.screen_follows_actor else True
+            #     # elif event.key == K_b:
+            #     #     # self.change_mode()
+            #     #     self.b = False if self.b else True
+            #     # elif event.key == K_f:
+            #     #     self.follow_mode = False if self.follow_mode else True
+            #     # elif event.key == K_l:
+            #     #     # self.change_mode()
+            #     #     self.locations[self.location]['lights on'] = False if self.locations[self.location]['lights on'] else True
+            #     # elif event.key == K_l:
+            #     #     # self.change_mode()
+            #     #     self.lights_on = False if self.lights_on else True
+            #     # elif event.key == K_m:
+            #     #     self.need_to_show_minimap = False if self.need_to_show_minimap else True
+            #     elif event.key == K_n:
+            #         # self.change_mode()
+            #         self.is_n = False if self.is_n else True
+            #         # msg = 'NEW EMPTY MESSAGE FOR TEST PURPOSES.'
+            #         # self.info_windows[0].get_bunch_of_new_messages((msg, msg))
+            #         # self.add_info_window(self.calculate_info_string_xy(), [msg, ], 300, False)
+            #
+            #     elif event.key == K_p:
+            #         # Cool stuff with if-then-else expression compress:
+            #         self.is_p = False if self.is_p else True
+            #     elif event.key == K_i:
+            #         # Cool stuff with if-then-else expression compress:
+            #         self.is_i = False if self.is_i else True
+            #     # elif event.key == K_SPACE:
+            #     #     self.change_player_actors()
+            #     # elif event.key == K_e:
+            #     # # elif event.key == K_KP_ENTER:
+            #     #     # elif event.key == K_SPACE:
+            #     #     # self.input_confirm = True
+            #     #     self.wandering_actor.end_turn()
+            #     #     self.player_turn = False
+            #     # elif event.key == K_c:
+            #     #     self.skip_actor()
+            #     # elif event.key == K_TAB:
+            #     #     self.need_to_show_party_inventory = True if not self.need_to_show_party_inventory else False
+            if event.type == MOUSEBUTTONDOWN:
+                buttons = pygame.mouse.get_pressed()
+                if buttons[0]:
+                    self.is_mouse_button_down = True
+                    self.is_left_mouse_button_down = True
+                if buttons[2]:
+                    self.is_mouse_button_down = True
+                    self.is_right_mouse_button_down = True
+            # elif event.type == MOUSEWHEEL:
+            #     # print(event)
+            #     # print(event.x, event.y)
+            #     # print(event.flipped)
+            #     # print(event.which)
+            #     self.is_mouse_wheel_rolls = True
+            #     if event.y == 1:
+            #         # Mouse wheel up:
+            #         self.is_mouse_wheel_up = True
+            #         # self.wandering_screen_target_scale += self.wandering_scale_amount
+            #     elif event.y == -1:
+            #         # Mouse wheel down:
+            #         self.is_mouse_wheel_down = True
+            if event.type == MOUSEBUTTONUP:
+                self.is_mouse_button_down = False
+                if self.is_right_mouse_button_down:
+                    self.is_right_mouse_button_down = False
+                if self.is_left_mouse_button_down:
+                    self.is_left_mouse_button_down = False
+
+    def add_obstacle(self, description):
+        entity = Obstacle()
+        entity.id = self.obstacle_id
+        entity.is_gravity_affected = True if 'is gravity affected' in description else False
+        entity.rectangle.topleft = description[0]
+        entity.rectangle.width = description[1][0]
+        entity.rectangle.height = description[1][1]
+        # entity.max_speed = 0.6
+        entity.is_move_right = True if 'move right' in description else False
+        entity.is_move_up = True if 'move up' in description else False
+        entity.is_move_down = True if 'move down' in description else False
+        entity.is_move_left = True if 'move left' in description else False
+        entity.is_ghost_platform = True if 'ghost' in description else False
+        # Add an obstacle to the world storage:
+        if self.location not in self.obstacles.keys():
+            self.obstacles[self.location] = dict()
+        self.obstacles[self.location][entity.id] = entity
+        self.obstacle_id += 1
+
+    def load(self):
+        for obs in locations[self.location]['obstacles']['platforms']:
+            self.add_obstacle(obs)
+        print(f'[world.load] loaded obstacles: {len(self.obstacles[self.location])}')
+
+    def render_background(self):
+        pygame.draw.rect(self.screen, BLACK, (0,0,MAXX, MAXY))
+
+    def render_obstacles(self):
+        for key in self.obstacles[self.location].keys():
+            obs = self.obstacles[self.location][key]
+            pygame.draw.rect(self.screen, WHITE, (obs.rectangle.x - self.camera.offset_x, obs.rectangle.y - self.camera.offset_y,
+                                                  obs.rectangle.width, obs.rectangle.height))
+
+    def process(self):
+        self.processing_human_input()
+        if self.camera_follows_mouse:
+            self.camera.apply_offset((self.mouse_xy[0], self.mouse_xy[1]),
+                                     10, 10)
+        self.render_background()
+        self.render_obstacles()
+
+
+world = World()
+world.set_screen(screen)
+world.load()
+def main():
+    while True:
+        world.process()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
--- main .py
+++ main .py
@@ -5,7 +5,7 @@ from world import *
 # from info_window import *
 # from actors_description import*
 # from load_content import *
-# from locations import *
+from locations import *
 # from sound import *
 # import fonts
 
@@ -39,7 +39,7 @@ fades_speed = 100
 # world.press_any_key()
 # black_out(world.screen, world.screen, 5)
 # ---------------------------------------------------
-world.location = 'default'
+world.location = '01'
 if world.location not in world.locations.keys():
     world.locations[world.location] = dict()
 player = {
@@ -47,34 +47,8 @@ player = {
 }
 world.add_actor(player)
 
-
-o = (
-    ((0, MAXY-50),(MAXX*3, 50)), # floor
-    ((300, MAXY-250),(300, 80)), # floor
-    # ((-100, 500), (150, 150), 'move right'),  # left wall
-    ((0, -50), (50, MAXY)),  # left wall giant
-    ((51, 550), (350, 150), 'move right'),  # left wall
-    ((MAXX*3 - 150, 0), (150, MAXY)),  # right wall
-    # steps:
-    ((600, MAXY - 60), (50, 200)),
-    ((650, MAXY - 70), (50, 200)),
-    ((700, MAXY - 80), (50, 200)),
-    ((750, MAXY - 90), (50, 200)),
-    # moving ghosts:
-    # ((1200, MAXY_DIV_2+250), (1000, 10), 'move left', 'ghost'),
-    ((1200, MAXY_DIV_2+250), (1000, 10), 'move left'),
-    # ((1200, MAXY_DIV_2+235), (1000, 10), 'ghost'),
-
-    # ((300, MAXY_DIV_2), (400, 50), False, False),
-    # ((MAXX_DIV_2, MAXY_DIV_2 + 150), (50, MAXY_DIV_2), False, False),
-    #
-    # ((MAXX - 50,0), (50, MAXY-200), False, False),
-    # ((MAXX - 300,0), (50, MAXY-300), False, False)
-)
-for obs in o:
+for obs in locations['01']['obstacles']['platforms']:
     world.add_obstacle(obs)
-
-
 
 def main():
     max_fps = 0
